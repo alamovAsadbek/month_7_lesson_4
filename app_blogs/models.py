f8fa10d990d04ac4254dcc4d88d90af9ba111a98
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-29 22:06:37 +0500

    change

--- app_blogs/models.py
+++ app_blogs/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from rest_framework import serializers
+
+from common.models import BaseModel
 
 
-class BlogModel(models.Model):
+class BlogModel(BaseModel):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    user = serializers.PrimaryKeyRelatedField(to='users.UserModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'blog'
+        verbose_name_plural = 'Blogs'
+        verbose_name = 'Blog'
