9372b03f0d52322ba7d092cb7f30317d0a191313
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-29 22:15:25 +0500

    change

--- app_comments/models.py
+++ app_comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+from app_blogs.models import BlogModel
+from app_users.models import UserModel
+from common.models import BaseModel
+
+
+class CommentModel(BaseModel):
+    content = models.TextField()
+    user = serializers.PrimaryKeyRelatedField(queryset='UserModel.objects.all()', required=True)
+    post = serializers.PrimaryKeyRelatedField(queryset='BlogModel.objects.all()', required=True)
+
+    class Meta:
+        db_table = 'app_comments'
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return self.content
