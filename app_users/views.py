3a587c1fcd32bd9cb01fd2d02e0364654862b9fd
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 00:43:42 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -1,8 +1,10 @@
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializer import *
 
 
+@api_view(['GET', 'POST'])
 def user_view(request):
     if request.method == 'GET':
         users = UserModel.objects.all()
