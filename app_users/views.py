d7a6bfa9cc58cd25104a23b738cbc87d485851c6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 01:05:06 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -21,24 +21,24 @@ def user_view(request):
 @api_view(['GET', 'PUT', 'DELETE', 'PATCH'])
 def user_detail_view(request, user_id):
     if request.method == 'GET':
-        user = UserModel.objects.filter(pk=pk)
+        user = UserModel.objects.filter(pk=user_id)
         serializer = UserModelSerializer(user)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data)
     elif request.method == 'PUT':
         data = request.data
-        user = UserModel.objects.get(pk=pk)
+        user = UserModel.objects.get(pk=user_id)
         serializer = UserModelSerializer(user, data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "User updated successfully", "data": serializer.data})
     elif request.method == 'DELETE':
-        user = UserModel.objects.get(pk=pk)
+        user = UserModel.objects.get(pk=user_id)
         user.delete()
         return Response({"message": "User deleted successfully"})
     elif request.method == 'PATCH':
         data = request.data
-        user = UserModel.objects.get(pk=pk)
+        user = UserModel.objects.get(pk=user_id)
         serializer = UserModelSerializer(user, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
