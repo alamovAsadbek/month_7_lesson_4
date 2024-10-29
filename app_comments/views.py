f0230838348fc9a28f76bdbb718527abba5332ea
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 01:23:44 +0500

    change

--- app_comments/views.py
+++ app_comments/views.py
@@ -1,11 +1,16 @@
 from rest_framework.decorators import api_view
-from .models import *
+from rest_framework.response import Response
+
+from .serializer import *
+
 
 @api_view(['GET', 'POST'])
 def comments_view(request):
     if request.method == 'GET':
-        comment=CommentModel.objects.all()
-        serializer=CommentSerializer(comment, many=True)
+        comment = CommentModel.objects.all()
+        serializer = CommentModelSerializer(comment, many=True)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         # Create a new comment
         pass
