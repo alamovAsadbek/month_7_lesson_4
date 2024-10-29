60bc04c8062833bbc3eaa1759ca44f6ea6f4e159
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 01:18:13 +0500

    change

--- app_comments/views.py
+++ app_comments/views.py
@@ -1,11 +1,11 @@
 from rest_framework.decorators import api_view
-
+from .models import *
 
 @api_view(['GET', 'POST'])
 def comments_view(request):
     if request.method == 'GET':
-        # Get all comments
-        pass
+        comment=CommentModel.objects.all()
+
     elif request.method == 'POST':
         # Create a new comment
         pass
