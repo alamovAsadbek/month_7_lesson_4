37271bd3ecf6ef8a8f9db1d7529651b00e8e1a0a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 00:41:38 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from app_blogs.views import *
+from app_users.views import *
 
 urlpatterns = [
-    path('blogs/', blogs_view, name='blogs')
+    path('blogs/', blogs_view, name='blogs'),
+    path('users/', user_view, name='users'),
+
 ]
