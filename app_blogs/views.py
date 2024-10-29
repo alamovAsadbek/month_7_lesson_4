69ebda849b5b89577a587dfbae20892e58fdf728
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-30 00:04:49 +0500

    change

--- app_blogs/views.py
+++ app_blogs/views.py
@@ -1,7 +1,16 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializer import *
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def blogs_view(request):
     if request.method == 'GET':
-        pass
+        blogs = BlogModel.objects.all()
+        serializer = BlogModelSerializer(blogs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BlogModelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({"message": "Blog created successfully", "data": serializer.data})
